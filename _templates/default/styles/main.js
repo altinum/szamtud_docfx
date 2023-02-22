8e61af4a273df639849ee35e0347fbb87fc37237
bodnarbalazs <74934278+bodnarbalazs@users.noreply.github.com>
2023-02-22 16:34:00 +0100

    Update main.js

--- _templates/default/styles/main.js
+++ _templates/default/styles/main.js
@@ -1,54 +1,51 @@
-//Movies
-    if (element.tagName === 'A') {
- 
-        if (endsWithAny(['.webm', '.mkv', '.ogv', '.ogg', '.avi', '.mp4', '.m4v'], element.href)) {
-            //alert(element.href);
-            try {
- 
-                if (element.nextElementSibling && element.nextElementSibling.classList.contains("innerVideoContainer")) return false;
- 
-                var t = document.getElementById("inlineVideo_template").innerHTML;
-                let elem = document.createElement("div");
-                elem.classList.add("innerVideoContainer");
-                elem.innerHTML = t;
-                elem.appendAfter(element);
-                console.log(elem);
-                elem.firstElementChild.src = element.href;
-                elem.lastElementChild.addEventListener("click", (e) => {
-                    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-                    //console.log(e.target.parentNode);
-                });
- 
-                elem.firstElementChild.addEventListener("play", (e) => {
-                    //console.log(`L play ${e.target.currentTime} ${e.target.src}` );
-                    
-                    if (ws && ws.readyState === 1) ws.send(`L play ${e.target.currentTime} ${e.target.src}`);
-                });
-                elem.firstElementChild.addEventListener("pause", (e) => {
-                    //console.log(`L pause ${e.target.currentTime} ${e.target.src}`);
-                    if (ws && ws.readyState === 1) ws.send(`L pause ${e.target.currentTime} ${e.target.src}`);
-                });
- 
-            } catch (e) {
-                console.error(e);
-            }
-            return false; // prevent default action and stop event propagation
-        }
-    }
- 
-    //Links in new tab
-    if (element.hash != null && element.hash.startsWith("#")) {
-        console.log("LOCAL " + element.hash);
-        location.hash = element.hash.substring(1);
-        return false;
-    }
-    if (element.href != null) {
-        console.log("Link clicked :" + element.href);
-        window.open(element.href);
-        e.preventDefault();
-        return false;
-    }
-    if (element.href === undefined) {
-        return true;
-    }
-};
+function translateTasks(){
+
+	let ps=document.querySelectorAll("p");
+
+	for(var i=0; i<ps.length; i++){
+		if(ps[i].innerHTML.indexOf("(+/-)")==0){
+			let t=document.createElement("span");
+			t.innerHTML="<span class=\"tg_cnt\">#"+i+"</span><span>✓</span><span>?</span>";
+			t.classList.add("tg_task");
+			t.classList.add("tg_taskctrl");
+			t.classList.add("tg_neutral");
+			t.id="task"+i;
+			
+			let text=ps[i].innerText;
+			text=text.substring(6,text.length-1);
+			ps[i].innerHTML="";
+			
+			ps[i].appendChild(t);
+			ps[i].innerHTML+=text;
+			ps[i].addEventListener("click", taskClicked);
+		}
+	}
+}
+function taskClicked(e) {
+    let el = e.target;
+    //console.log(el);
+    if (!el.id) {
+        el = el.parentNode; //
+    }
+
+    let ele = el; //.firstChild
+    console.log(e.target.id + " was clicked");
+    let newStatus = "";
+    if (ele.classList.contains("tg_neutral")) {
+        ele.classList.remove("tg_neutral");
+        ele.classList.add("tg_success");
+        newStatus = "+";
+
+    } else
+        if (ele.classList.contains("tg_success")) {
+            ele.classList.remove("tg_success");
+            ele.classList.add("tg_fail");
+            newStatus = "-";
+        } else if (ele.classList.contains("tg_fail")) {
+            ele.classList.remove("tg_fail");
+            ele.classList.add("tg_neutral");
+            newStatus = "0";
+        }
+
+    ws.send(`S ${el.id.substring(4)} ${newStatus}`);
+}
