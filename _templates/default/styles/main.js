168d85423737f38ac2e9b94e5045b45dfa56acf6
bodnarbalazs <74934278+bodnarbalazs@users.noreply.github.com>
2023-03-11 23:27:59 +0100

    Update main.js

--- _templates/default/styles/main.js
+++ _templates/default/styles/main.js
@@ -97,6 +97,7 @@ function preprocess(){
 			if(children[i].innerHTML.indexOf("(!EndStepper)")==0){
 				   	children[i].innerHTML="";
 					$("#stepper-"+stepperCount)[0].firstChild.innerHTML=document.querySelector(".stepDescription", ".step-1","stepperStep-"+stepCount).innerHTML.substring(8,document.querySelector(".stepDescription", ".step-1","stepperStep-"+stepCount).innerHTML.length-1);
+					stepCount=0;
 				}
 		}
 	}
