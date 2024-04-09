7e2e64a90b832c8a0657988a794712c9019b2e2e
Mohácsi László <mohacsi@altinum.hu>
2024-04-09 22:00:57 +0200

    7. hét giffel

---
+++ softec/csv_databinding/csv_dababinding/csv_dababinding/bin/Debug/net6.0-windows7.0/CountryData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csv_dababinding
+{
+public class CountryData
+{
+    public string Name { get; set; } = string.Empty;
+    public long Population { get; set; }
+    public double AreaInSquareKm { get; set; }
+}
+}
